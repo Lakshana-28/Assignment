601d4eebdc6879b2e9396a9c0a93fe1f7f4142ea
Lakshana-28 <34645086+Lakshana-28@users.noreply.github.com>
2018-01-09 06:32:33 +0000

    Create decimal-binary.c

---
+++ decimal-binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+  int i,num,a[111];
+  scanf("%d",&num);
+  for(i=0;n>0;i++)
+  {
+    a[i]=n%2;
+    n=n/2;
+  }
+  for(i=i-1;i>=n;i--)
+  {
+    printf("%d",a[i])
+  }
+}
+
